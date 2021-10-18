42b656c0642ec973c297c055b349dfea25c9014d
GunHee Lee <44331507+jojo9103@users.noreply.github.com>
2021-10-18 09:40:43 +0900

    Create range.R

---
+++ range.R
@@ -0,0 +1,52 @@
+val<-function(seq_num,seq1=NULL){
+  seq_num<-seq_num[!duplicated(seq_num)]
+  con_num<-c()
+  con<-c()
+  if(is.null(seq1)){
+    for(i in 1:(length(seq_num))){
+      # print(i)
+      if(i!=length(seq_num)){
+        i1=seq_num[i]+1
+        if(i1!=seq_num[i+1]){
+          
+          if(is.null(con)){con_num<-c(con_num,(seq_num[i]))}
+          if(!is.null(con)){con_num=c(con_num,paste0(con,'-',(seq_num[i])));con=NULL}
+          
+        }
+        if(i1==seq_num[i+1]){
+          if(is.null(con)){con=seq_num[i]}
+        }
+      }
+      
+      if(i==length(seq_num)){
+        if(!is.null(con)){con_num=c(con_num,paste0(con,'-',(seq_num[i])))}
+        if(is.null(con)){con_num<-c(con_num,(seq_num[i]))}
+        
+      }
+    }
+  }
+  if(!is.null(seq1)){
+    for(i in 1:(length(seq_num))){
+      # print(i)
+      if(i!=length(seq_num)){
+        i1=seq_num[i]+1
+        if(i1!=seq_num[i+1]){
+          if(is.null(con)){con_num<-c(con_num,seq1[seq_num[i]])}
+          if(!is.null(con)){con_num=c(con_num,paste0(c(con,seq1[seq_num[i]]),collapse = ''));con=NULL}
+          
+        }
+        if(i1==seq_num[i+1]){
+          if(!is.null(con)){con=c(con,seq1[seq_num[i]])}
+          if(is.null(con)){con=c(seq1[seq_num[i]])}
+        }
+      }
+      
+      if(i==length(seq_num)){
+        if(!is.null(con)){con_num=c(con_num,paste0(c(con,seq1[seq_num[i]]),collapse = ''))}
+        if(is.null(con)){con_num<-c(con_num,(seq1[seq_num[i]]))}
+        
+      }
+    }
+  }
+  return(con_num)
+} 
